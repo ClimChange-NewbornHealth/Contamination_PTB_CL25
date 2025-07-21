7dc606d1e3812e38cf96820bed79a5930e99901e
José Daniel Conejeros <43749364+JDConejeros@users.noreply.github.com>
2025-07-21 12:48:59 -0400

    UP study contant

--- Code/1.0 Process_data_contam.R
+++ Code/1.0 Process_data_contam.R
@@ -76,3 +76,122 @@ summary(data)
 
 # Save data ---------
 save(data, file=paste0(data_out, "series_pm25_o3_kriging_idw", ".RData"))
+
+# Descriptive analysis (vs real station values) ---------
+glimpse(data)
+load(file=paste0(data_inp, "analytical_series_full_2000_2023", ".RData"))
+glimpse(series)
+
+g0 <- ggplot(series, aes(x = pm25)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "darkgreen", color = "darkgreen") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "PM2.5") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 100))
+
+g0
+
+g1 <- ggplot(data, aes(x = pm25_krg)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "red", color = "red") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "PM2.5 Kriging") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 100))
+
+g1
+
+
+g2 <- ggplot(data, aes(x = pm25_idw)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "blue", color = "blue") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "PM2.5 IDW") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 100))
+
+g2
+
+g0b <- ggplot(series, aes(x = o3)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "darkgreen", color = "darkgreen") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "Ozone") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 70))
+
+g0b
+
+g1b <- ggplot(data, aes(x = o3_krg)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "red", color = "red") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "Ozone Kriging") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 70))
+
+g1b
+
+
+g2b <- ggplot(data, aes(x = o3_idw)) +
+    geom_density(alpha = 0.3, linewidth = 0.8, fill = "blue", color = "blue") +
+    #scale_fill_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    #scale_color_manual(values = c("Kriging" = "#1f77b4", "IDW" = "#ff7f0e")) +
+    labs(x = "PM2.5", y = "Density", title = "Ozone IDW") +
+    theme_light() +
+    theme(
+      panel.grid = element_blank(),
+      legend.position = "top",
+      legend.title = element_blank(),
+      plot.title = element_text(size = 11, hjust = 0)
+    ) +
+    coord_cartesian(xlim = c(0, 70))
+
+g2b
+
+
+ggarrange(g0, g1, g2, 
+          g0b, g1b, g2b, 
+          nrow = 2, ncol = 3)
+
+ggsave(
+  filename = "Output/Descriptives/Distribution_contamination.png",
+  #plot     = last_plot(),
+  res      = 300,
+  width    = 30,
+  height   = 20,
+  units    = 'cm',
+  scaling  = 0.9,
+  device   = ragg::agg_png
+)
